4271c03e98156eb89d8d27841671183fb9380719
shermainey <sher33k@gmail.com>
2026-04-13 13:24:22 +0800

    Delete src directory

--- src/models/material.py
+++
@@ -1,8 +0,0 @@
-class Material:
-    def __init__(self, grade, fy, cost):
-        self.grade = grade
-        self.fy = float(fy)
-        self.cost = float(cost)
-
-    def __str__(self):
-        return f"Material(grade={self.grade}, fy={self.fy}, cost={self.cost})"
